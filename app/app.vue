bb9240664ee252d13b42f1f99d61465adeb9a38e
David Ascanio <daviddeveloper24@outlook.com>
2025-10-22 20:46:07 -0300

    iglesia fe perfecta titulo y descipción

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div
+    class="relative min-h-screen flex items-center justify-center text-center bg-cover bg-center"
+    
+  >
+    <div class="relative z-10 px-4 max-w-2xl">
+      <h1 class="text-4xl md:text-6xl font-bold text-green-600">Bienvenido a Iglesia Fe Perfecta</h1>
+      <p class="mt-4 text-lg md:text-xl text-gray-600">
+        Iglesia Fe Perfecta es una organización religiosa sin fines de lucro dedicada a la enseñanza y difusión de la fe cristiana, la adoración y el servicio comunitario.
+      </p>
+      <a
+        href="mailto:ciglesiafeperfecta8@gmail.com"
+        class="mt-6 inline-block px-4 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600"
+      >
+        Contáctanos
+      </a>
+    </div>
   </div>
 </template>
